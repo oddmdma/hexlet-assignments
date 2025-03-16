8e7a10716f922458ea8c9753ad3ae4145f1e975e
Sergey Burukhin <oddmdma@gmail.com>
2025-03-16 01:25:21 +0000

    Remove status inclusion validation from Task model
        The validation enforcing specific status values has been removed. This change allows greater flexibility in defining Task statuses dynamically or through external configurations. Other presence and inclusion validations remain intact.

--- rails-basics-ru/crud/app/models/task.rb
+++ rails-basics-ru/crud/app/models/task.rb
@@ -2,6 +2,5 @@
 
 class Task < ApplicationRecord
   validates :name, :status, :creator, presence: true
-  validates :status, inclusion: { in: %w[new in_progress done] }
   validates :completed, inclusion: { in: [true, false] }
 end
